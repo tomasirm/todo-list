03307cc941411aff07827dc2f706ffb8b864481a
Tomas <triquelme@creditu.com>
2020-10-21 00:32:35 -0300

    first commit

---
+++ lib/todo_list_web/api/schema.ex
@@ -0,0 +1,11 @@
+defmodule TodolistWeb.Api.Schema do
+  use Absinthe.Schema
+
+  query do
+    field :hello, :string do
+      resolve(fn _, _ ->
+      {:ok, "Hello World"}
+      end)
+    end
+  end
+end
